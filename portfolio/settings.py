e26048bd0140d304800f88a0a8c8e1fda2af6a97
Kaushal kumar <kaushalkushwah1356@gmail.com>
2026-03-20 19:55:43 +0530

    -

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fgvhxd^(4t)$j2xbkfw*1#+&(&(tc5wr-u3rk)f!#g(49%u%#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'yourusername.pythonanywhere.com', 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = [ 'kaushal1356.pythonanywhere.com', 'localhost', '127.0.0.1' ]
 
 
 # Application definition
